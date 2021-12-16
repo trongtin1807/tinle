e7031f114ab765adcfb53d5e0c6976f141d74496
trongtin2000 <tin.le1807@gmail.com>
2021-12-16 11:48:49 +0700

    m

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+include_once './config.php';
+include './pdo.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Book Store Template, Free CSS Template, CSS Website Layout</title>
+<meta name="keywords" content="Book Store Template, Free CSS Template, CSS Website Layout, CSS, HTML" />
+<meta name="description" content="Book Store Template, Free CSS Template, Download CSS Website" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<!--  Free CSS Templates from www.templatemo.com -->
+    <?php include './subpage/menu.php' ?> <!-- end of menu -->
+    
+    <?php include './subpage/header.php' ?> <!-- end of header -->
+    
+   <?php include './subpage/content_left.php' ?> <!-- end of content left -->
+        
+   <?php  include './subpage/content_right.php' ?> <!-- end of content -->
+   
+    <?php include './subpage/footer.php' ?> <!-- end of container -->
+</body>
+</html>
